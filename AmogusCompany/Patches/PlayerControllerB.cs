f0bd951f88149665d592e0b9d218501616b69fd6
ijustride <artekovec@outlook.com>
2024-01-28 19:53:49 +0700

    Implemented killing logic

--- AmogusCompany/Patches/PlayerControllerB.cs
+++ AmogusCompany/Patches/PlayerControllerB.cs
@@ -5,33 +5,45 @@ using LC_API.GameInterfaceAPI.Features;
 using UnityEngine;
 using TMPro;
 
-namespace AmogusCompanyMod.Patches {
+namespace AmogusCompanyMod.Patches
+{
     [HarmonyPatch(typeof(PlayerControllerB))]
-    class PlayerControllerBPatch {
+    class PlayerControllerBPatch
+    {
         [HarmonyPatch("Update")]
         [HarmonyPostfix]
-        static public void PlayerControllerUpdate() {
+        static public void PlayerControllerUpdate()
+        {
             IEnumerator<Player> activePlayers = Player.ActiveList.GetEnumerator();
-            while (activePlayers.MoveNext()) {
+            while (activePlayers.MoveNext())
+            {
                 if (AmogusModBase.impostorsIDs.Contains(activePlayers.Current.ClientId) && activePlayers.Current.IsControlled &&
-                    (AmogusModBase.impostorsIDs.Contains(Player.LocalPlayer.ClientId))) {
+                    (AmogusModBase.impostorsIDs.Contains(Player.LocalPlayer.ClientId)))
+                {
                     activePlayers.Current.PlayerController.usernameBillboardText.canvasRenderer.SetColor(UnityEngine.Color.red);
                     activePlayers.Current.PlayerController.usernameAlpha.alpha = 1f;
-                } else {
+                }
+                else
+                {
                     activePlayers.Current.PlayerController.usernameBillboardText.canvasRenderer.SetColor(UnityEngine.Color.white);
                 }
             }
 
-            try {
-                if (AmogusModBase.impostorsIDs.Contains(Player.LocalPlayer.ClientId)) {
+            try
+            {
+                if (AmogusModBase.impostorsIDs.Contains(Player.LocalPlayer.ClientId))
+                {
                     Player.LocalPlayer.PlayerController.sprintMeter = 1f;
                     Player.LocalPlayer.PlayerController.nightVision.enabled = true;
                 }
-            } catch {
+            }
+            catch
+            {
                 AmogusModBase.mls.LogInfo("Failed to get Player.LocalPlayer.ClientId");
             }
 
-            if (BepInEx.UnityInput.Current.GetKeyDown("F9")) {
+            if (BepInEx.UnityInput.Current.GetKeyDown("F9"))
+            {
                 AmogusModBase.DebugMode = true;
                 AmogusModBase.mls.LogInfo("Debug Mode Toggle: " + AmogusModBase.DebugMode);
             }
@@ -39,30 +51,39 @@ namespace AmogusCompanyMod.Patches {
 
         [HarmonyPatch("KillPlayerClientRpc")]
         [HarmonyPostfix]
-        static public void KillPlayerClientRpcPatch() {
+        static public void KillPlayerClientRpcPatch()
+        {
             OtherFunctions.CheckForImpostorVictory();
         }
 
         [HarmonyPatch("KillPlayerServerRpc")]
         [HarmonyPostfix]
-        static public void KillPlayerServerRpcPatch() {
+        static public void KillPlayerServerRpcPatch()
+        {
             OtherFunctions.CheckForImpostorVictory();
         }
 
         [HarmonyPatch("SetHoverTipAndCurrentInteractTrigger")]
         [HarmonyPostfix]
-        static public void TooltipPatch(PlayerControllerB __instance) {
-            if (!AmogusModBase.impostorsIDs.Contains(Player.LocalPlayer.ClientId)) {
+        static public void TooltipPatch(PlayerControllerB __instance)
+        {
+            if (!AmogusModBase.impostorsIDs.Contains(Player.LocalPlayer.ClientId))
+            {
                 return;
             }
             var interactRay = new Ray(__instance.gameplayCamera.transform.position, __instance.gameplayCamera.transform.forward);
-            if (!__instance.isFreeCamera && Physics.Raycast(interactRay, out var hit, 5f, 8)) {
+            if (!__instance.isFreeCamera && Physics.Raycast(interactRay, out var hit, 5f, 8))
+            {
                 PlayerControllerB playerLookingAt = hit.collider.gameObject.GetComponent<PlayerControllerB>();
-                if (playerLookingAt != null && __instance.playerClientId != playerLookingAt.playerClientId) {
+                if (playerLookingAt != null && __instance.playerClientId != playerLookingAt.playerClientId)
+                {
                     var ellapsed = Time.time - AmogusModBase.lastKillTime;
-                    if (ellapsed >= 60) {
+                    if (ellapsed >= 60)
+                    {
                         __instance.cursorTip.text = "KILL " + playerLookingAt.playerUsername;
-                    } else {
+                    }
+                    else
+                    {
                         __instance.cursorTip.text = $"KILL on cooldown: {(int)(60 - ellapsed)}s";
                     }
                 }
@@ -71,25 +92,33 @@ namespace AmogusCompanyMod.Patches {
 
         [HarmonyPatch("BeginGrabObject")]
         [HarmonyPrefix]
-        static public bool GrabObjectPatch(PlayerControllerB __instance) {
+        static public bool GrabObjectPatch(PlayerControllerB __instance)
+        {
             AmogusModBase.mls.LogMessage("Trying to do the sussy");
             // Only impostor can kill
-            if (!AmogusModBase.impostorsIDs.Contains(Player.LocalPlayer.ClientId)) {
+            if (!AmogusModBase.impostorsIDs.Contains(Player.LocalPlayer.ClientId))
+            {
                 return true;
             }
             // Check for cooldown
             var ellapsed = Time.time - AmogusModBase.lastKillTime;
             AmogusModBase.mls.LogMessage($"Ellapsed since last kill: {ellapsed}");
-            if (ellapsed < 60) {
+            if (ellapsed < 60)
+            {
                 return true;
             }
             // Find the target player and kill
             var interactRay = new Ray(__instance.gameplayCamera.transform.position, __instance.gameplayCamera.transform.forward);
-            if (Physics.Raycast(interactRay, out var hit, 5f, 8)) {
+            if (Physics.Raycast(interactRay, out var hit, 5f, 8))
+            {
                 PlayerControllerB playerLookingAt = hit.collider.gameObject.GetComponent<PlayerControllerB>();
-                if (playerLookingAt != null && __instance.playerClientId != playerLookingAt.playerClientId) {
+                if (playerLookingAt != null && __instance.playerClientId != playerLookingAt.playerClientId)
+                {
                     AmogusModBase.mls.LogMessage("Killing player: " + playerLookingAt.playerUsername);
-                    playerLookingAt.KillPlayer(Vector3.zero, true);
+                    //playerLookingAt.KillPlayer(Vector3.zero, spawnBody: true, CauseOfDeath.Mauling, 0);
+                    playerLookingAt.DamagePlayerFromOtherClientServerRpc(100, Vector3.zero, (int)Player.LocalPlayer.ClientId);
+                    AmogusModBase.mls.LogMessage("Reached KillPlayer method" + (int)Player.LocalPlayer.ClientId);
+
                     AmogusModBase.lastKillTime = Time.time;
                     return false;
                 }
